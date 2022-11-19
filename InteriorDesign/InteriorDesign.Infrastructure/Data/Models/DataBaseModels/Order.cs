c8b6d37d0975777f160b724372cd95667c50aeae
mikegsCoder <mikegsid@gmail.com>
2022-11-19 11:50:01 +0200

    Add Order db model

---
+++ InteriorDesign/InteriorDesign.Infrastructure/Data/Models/DataBaseModels/Order.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InteriorDesign.Infrastructure.Data.Models.DataBaseModels
+{
+    public class Order : BaseDeletableModel<string>
+    {
+        public Order()
+        {
+            this.Id = Guid.NewGuid();
+            this.ConfiguredProducts = new List<ConfiguredProduct>();
+        }
+
+        [Required]
+        [MaxLength(20)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Phone]
+        public string Phone { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string DeliveryAddress { get; set; }
+
+        [MaxLength(200)]
+        public string? AdditionalDetails { get; set; }
+
+        [Required]
+        [Column(TypeName = "money")]
+        [Precision(18, 2)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string ApplicationUserId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(ApplicationUserId))]
+        public ApplicationUser ApplicationUser { get; set; }
+
+        [Required]
+        public bool IsShipped { get; set; }
+
+        public DateTime? ShippedOn { get; set; }
+
+        public virtual ICollection<ConfiguredProduct> ConfiguredProducts { get; set; }
+    }
+}
