27267001243996bf0d8b436e9a8442730315ab3e
mikegsCoder <mikegsid@gmail.com>
2022-11-19 11:48:23 +0200

    Add BaseDeletableModel

---
+++ InteriorDesign/InteriorDesign.Infrastructure/Data/Models/BaseDeletableModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InteriorDesign.Infrastructure.Data.Models
+{
+    public abstract class BaseDeletableModel<TKey> : BaseModel<TKey>, IDeletableEntity
+    {
+        public bool IsDeleted { get; set; }
+
+        //[Column(TypeName = "date")]
+        public DateTime? DeletedOn { get; set; }
+    }
+}
