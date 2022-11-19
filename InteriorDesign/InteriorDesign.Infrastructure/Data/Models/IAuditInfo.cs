214264f313b2b939bbb6064fb517861f2a9b8883
mikegsCoder <mikegsid@gmail.com>
2022-11-19 11:44:49 +0200

    Add IAuditInfo

---
+++ InteriorDesign/InteriorDesign.Infrastructure/Data/Models/IAuditInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace InteriorDesign.Infrastructure.Data.Models
+{
+    public interface IAuditInfo
+    {
+        DateTime CreatedOn { get; set; }
+
+        DateTime? ModifiedOn { get; set; }
+    }
+}
