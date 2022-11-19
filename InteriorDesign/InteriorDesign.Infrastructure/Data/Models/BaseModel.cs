355487357bb7f1e5289e05a492374092d0fa726f
mikegsCoder <mikegsid@gmail.com>
2022-11-19 11:46:55 +0200

    Add BaseModel

---
+++ InteriorDesign/InteriorDesign.Infrastructure/Data/Models/BaseModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InteriorDesign.Infrastructure.Data.Models
+{
+    public abstract class BaseModel<TKey> : IAuditInfo
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        //[Column(TypeName = "date")]
+        public DateTime CreatedOn { get; set; }
+
+        //[Column(TypeName = "date")]
+        public DateTime? ModifiedOn { get; set; }
+    }
+}
