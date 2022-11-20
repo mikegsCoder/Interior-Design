0c1a4e296f3458f40dd562e15258ddc016a1aed6
mikegsCoder <mikegsid@gmail.com>
2022-11-20 12:02:38 +0200

    Add Product db model

---
+++ InteriorDesign/InteriorDesign.Infrastructure/Data/Models/DataBaseModels/Product.cs
@@ -0,0 +1,41 @@
+﻿using InteriorDesign.Infrastructure.Constants;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InteriorDesign.Infrastructure.Data.Models.DataBaseModels
+{
+    public class Product : BaseDeletableModel<string>
+    {
+        public Product()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        [Required]
+        [StringLength(DataConstants.ProductNameMaxLength)]
+        public string Name { get; set; }
+
+        [Required]
+        public Guid ModelId { get; set; }
+
+        [ForeignKey(nameof(ModelId))]
+        public ProductModel Model { get; set; }
+
+        [Required]
+        public Guid ColorId { get; set; }
+
+        [ForeignKey(nameof(ColorId))]
+        public ProductColor Color { get; set; }
+
+        [Required]
+        [Column(TypeName = "money")]
+        [Range(DataConstants.ProductPriceMinRange, DataConstants.ProductPriceMaxRange)]
+        [Precision(DataConstants.ProductPricePrecision, DataConstants.ProductPriceScale)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [MaxLength(DataConstants.ProductImageUrlMaxLength)]
+        public string ImageUrl { get; set; }
+    }
+}
