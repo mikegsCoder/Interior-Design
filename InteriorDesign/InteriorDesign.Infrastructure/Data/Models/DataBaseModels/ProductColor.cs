1fdea3909b82d853ba770c8a85be9d4027634fee
mikegsCoder <mikegsid@gmail.com>
2022-11-20 12:03:54 +0200

    Add ProductColor db model

---
+++ InteriorDesign/InteriorDesign.Infrastructure/Data/Models/DataBaseModels/ProductColor.cs
@@ -0,0 +1,20 @@
+﻿using InteriorDesign.Infrastructure.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace InteriorDesign.Infrastructure.Data.Models.DataBaseModels
+{
+    public class ProductColor : BaseDeletableModel<string>
+    {
+        public ProductColor()
+        {
+            this.Id = Guid.NewGuid();
+            this.Products = new HashSet<Product>();
+        }
+
+        [Required]
+        [StringLength(DataConstants.ProductColorNameMaxLength)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
