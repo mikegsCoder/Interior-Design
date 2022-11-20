6cd9d513e6da4b6cc6fbeecc1a6633b569f9ad61
mikegsCoder <mikegsid@gmail.com>
2022-11-20 12:01:22 +0200

    Add ConfiguredProduct db model

---
+++ InteriorDesign/InteriorDesign.Infrastructure/Data/Models/DataBaseModels/ConfiguredProduct.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InteriorDesign.Infrastructure.Data.Models.DataBaseModels
+{
+    public class ConfiguredProduct : BaseDeletableModel<string>
+    {
+        public ConfiguredProduct()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        [Required]
+        public Guid ProductId { get; set; }
+
+        [ForeignKey(nameof(ProductId))]
+        public virtual Product Product { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int Quantity { get; set; }
+
+        [Required]
+        [Column(TypeName = "money")]
+        [Precision(18, 2)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public bool IsOrdered { get; set; }
+
+        public DateTime? OrderedOn { get; set; }
+    }
+}
