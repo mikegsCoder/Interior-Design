0b9ae29e4769950e89e52fbbb15cb3d1502687b4
mikegsCoder <mikegsid@gmail.com>
2022-11-21 13:57:20 +0200

    Add ProductCategory db model

---
+++ InteriorDesign/InteriorDesign.Infrastructure/Data/Models/DataBaseModels/ProductCategory.cs
@@ -0,0 +1,25 @@
+﻿using InteriorDesign.Infrastructure.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace InteriorDesign.Infrastructure.Data.Models.DataBaseModels
+{
+    public class ProductCategory : BaseDeletableModel<string>
+    {
+        public ProductCategory()
+        {
+            Id = Guid.NewGuid().ToString();
+            ProductTypes = new HashSet<ProductType>();
+        }
+
+        [Required]
+        [StringLength(DataConstants.ProductCategoryNameMaxLength)]
+        public string Name { get; set; }
+
+        [Required]
+        [Url]
+        [StringLength(DataConstants.ProductCategoryImageUrlMaxLength)]
+        public string ImageUrl { get; set; }
+
+        public virtual ICollection<ProductType> ProductTypes { get; set; }
+    }
+}
