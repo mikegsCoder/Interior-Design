f4e98da594e7ee7c553b17fcc9bd62355acf9d6a
mikegsCoder <mikegsid@gmail.com>
2022-11-21 14:02:40 +0200

    Add Testimonial db model

---
+++ InteriorDesign/InteriorDesign.Infrastructure/Data/Models/DataBaseModels/Testimonial.cs
@@ -0,0 +1,27 @@
+﻿using InteriorDesign.Infrastructure.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace InteriorDesign.Infrastructure.Data.Models.DataBaseModels
+{
+    public class Testimonial : BaseDeletableModel<string>
+    {
+        public Testimonial()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        [Required]
+        [MaxLength(DataConstants.TestimonialAuthorMaxLength)]
+        public string Author { get; set; }
+
+        [Required]
+        [MaxLength(DataConstants.TestimonialTitleMaxLength)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(DataConstants.TestimonialContentMaxLength)]
+        public string Content { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
