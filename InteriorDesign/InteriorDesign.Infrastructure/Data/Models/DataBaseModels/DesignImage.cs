3928dd23111100feabda170cb8749cd294119877
mikegsCoder <mikegsid@gmail.com>
2022-11-21 13:59:54 +0200

    Add DesignImage db model

---
+++ InteriorDesign/InteriorDesign.Infrastructure/Data/Models/DataBaseModels/DesignImage.cs
@@ -0,0 +1,24 @@
+﻿using InteriorDesign.Infrastructure.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace InteriorDesign.Infrastructure.Data.Models.DataBaseModels
+{
+    public class DesignImage : BaseDeletableModel<string>
+    {
+        public DesignImage()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        [Required]
+        [Url]
+        [MaxLength(DataConstants.ImageUrlMaxLength)]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [MaxLength(DataConstants.ImageNameMaxLength)]
+        public string Name { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
