e3af64002c1201bfe4df0412eb9534e935a993be
mikegsCoder <mikegsid@gmail.com>
2022-11-21 14:04:35 +0200

    Add Contact db model

---
+++ InteriorDesign/InteriorDesign.Infrastructure/Data/Models/DataBaseModels/Contact.cs
@@ -0,0 +1,27 @@
+﻿using InteriorDesign.Infrastructure.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace InteriorDesign.Infrastructure.Data.Models.DataBaseModels
+{
+    public class Contact : BaseDeletableModel<string>
+    {
+        public Contact()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        [Required]
+        [MaxLength(DataConstants.ContactFromMaxLength)]
+        public string From { get; set; }
+
+        [Required]
+        [MaxLength(DataConstants.ContactSubjectMaxLength)]
+        public string Subject { get; set; }
+
+        [Required]
+        [MaxLength(DataConstants.ContsctMessageMaxLength)]
+        public string Message { get; set; }
+
+        public bool IsAnswered { get; set; }
+    }
+}
