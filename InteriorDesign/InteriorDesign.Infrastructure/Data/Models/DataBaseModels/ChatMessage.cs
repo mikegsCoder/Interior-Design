b897c0fb93fabfe0a99adbe88acadf6b0eddaade
mikegsCoder <mikegsid@gmail.com>
2022-11-21 13:58:50 +0200

    Add ChatMessage db model

---
+++ InteriorDesign/InteriorDesign.Infrastructure/Data/Models/DataBaseModels/ChatMessage.cs
@@ -0,0 +1,21 @@
+﻿using InteriorDesign.Infrastructure.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace InteriorDesign.Infrastructure.Data.Models.DataBaseModels
+{
+    public class ChatMessage : BaseDeletableModel<string>
+    {
+        public ChatMessage()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        [Required]
+        [MaxLength(DataConstants.ChatSenderMaxLength)]
+        public string Sender { get; set; }
+
+        [Required]
+        [MaxLength(DataConstants.ChatMessageMaxLength)]
+        public string Message { get; set; }
+    }
+}
