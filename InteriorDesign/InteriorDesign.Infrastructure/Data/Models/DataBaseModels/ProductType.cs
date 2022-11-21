ab4b1f4e6467528af2c431347524fa185a7f2d6d
mikegsCoder <mikegsid@gmail.com>
2022-11-21 13:56:19 +0200

    Add ProductType db model

---
+++ InteriorDesign/InteriorDesign.Infrastructure/Data/Models/DataBaseModels/ProductType.cs
@@ -0,0 +1,23 @@
+﻿using InteriorDesign.Infrastructure.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace InteriorDesign.Infrastructure.Data.Models.DataBaseModels
+{
+    public class ProductType : BaseDeletableModel<string>
+    {
+        public ProductType()
+        {
+            Id = Guid.NewGuid().ToString();
+            ProductCategories = new HashSet<ProductCategory>();
+            ProductModels = new HashSet<ProductModel>();
+        }
+
+        [Required]
+        [StringLength(DataConstants.ProductTypeNameMaxLength)]
+        public string Name { get; set; }
+
+        public virtual ICollection<ProductCategory> ProductCategories { get; set; }
+
+        public virtual ICollection<ProductModel> ProductModels { get; set; }
+    }
+}
