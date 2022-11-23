556c7b7fc394579503b747aa57d9d3a19b43caa7
mikegsCoder <mikegsid@gmail.com>
2022-11-23 18:44:29 +0200

    Add Seeder

---
+++ InteriorDesign/InteriorDesign.Web/InitialSeed/Seeder.cs
@@ -0,0 +1,27 @@
+﻿using InteriorDesign.Infrastructure.Data.Models.DataBaseModels;
+using InteriorDesign.Infrastricture.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace InteriorDesign.Web.InitialSeed
+{
+    public class Seeder
+    {
+        public static async Task SeedAsync(IApplicationBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            using var serviceScope = builder.ApplicationServices.CreateScope();
+
+            var dbContext = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+            
+            dbContext.Database.Migrate();
+
+            var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+        }
+    }
+}
