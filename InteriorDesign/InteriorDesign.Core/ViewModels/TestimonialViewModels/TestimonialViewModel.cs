b3670589616a85696a669fa24aef6335981d021f
mikegsCoder <mikegsid@gmail.com>
2022-11-25 17:19:25 +0200

    Add TestimonialViewModel

---
+++ InteriorDesign/InteriorDesign.Core/ViewModels/TestimonialViewModels/TestimonialViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace InteriorDesign.Core.ViewModels.TestimonialViewModels
+{
+    public class TestimonialViewModel
+    {
+        public string TestimonialId { get; set; } = null!;
+
+        public string Author { get; set; } = null!;
+
+        public string Title { get; set; } = null!;
+
+        public string Content { get; set; } = null!;
+
+        public bool IsActive { get; set; }
+    }
+}
