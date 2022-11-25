596ec21694adf5e531ea71ffc6a10e13434c2cf7
mikegsCoder <mikegsid@gmail.com>
2022-11-25 17:07:39 +0200

    Add AboutUsController

---
+++ InteriorDesign/InteriorDesign.Web/Controllers/AboutUsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InteriorDesign.Web.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
