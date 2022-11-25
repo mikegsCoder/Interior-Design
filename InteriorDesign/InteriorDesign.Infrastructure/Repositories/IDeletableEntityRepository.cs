f377efecf3d1a4d1a3703263fec2138e3e3b5ccb
mikegsCoder <mikegsid@gmail.com>
2022-11-25 17:21:08 +0200

    Add Repositories

---
+++ InteriorDesign/InteriorDesign.Infrastructure/Repositories/IDeletableEntityRepository.cs
@@ -0,0 +1,16 @@
+﻿using InteriorDesign.Infrastructure.Data.Models;
+
+namespace InteriorDesign.Infrastructure.Repositories
+{
+    public interface IDeletableEntityRepository<TEntity> : IRepository<TEntity>
+        where TEntity : class, IDeletableEntity
+    {
+        IQueryable<TEntity> AllWithDeleted();
+
+        IQueryable<TEntity> AllAsNoTrackingWithDeleted();
+
+        void HardDelete(TEntity entity);
+
+        void Undelete(TEntity entity);
+    }
+}
