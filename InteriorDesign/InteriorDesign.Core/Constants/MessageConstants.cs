bfebc8d94820503a695e075d4fc3f0363e6b1f2a
mikegsCoder <mikegsid@gmail.com>
2022-11-26 13:11:50 +0200

    Add MessageConstants

---
+++ InteriorDesign/InteriorDesign.Core/Constants/MessageConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace InteriorDesign.Core.Constants
+{
+    public static class MessageConstants
+    {
+        public const string Success = "success";
+        public const string Failed = "failed";
+    }
+}
