1c96bab0c6e96baf61ba8620b18e448421a25a71
mikegsCoder <mikegsid@gmail.com>
2022-11-26 12:58:41 +0200

    Add ClaimsPrincipalExtension

---
+++ InteriorDesign/InteriorDesign.Web/Extensions/ClaimsPrincipalExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace InteriorDesign.Web.Extensions
+{
+    public static class ClaimsPrincipalExtension
+    {
+        public static string Id(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
