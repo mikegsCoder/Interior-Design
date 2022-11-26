41bf5daa1bcba2c9adefa203bfd926a2cc9e18f5
mikegsCoder <mikegsid@gmail.com>
2022-11-26 13:10:57 +0200

    Add RoleConstants

---
+++ InteriorDesign/InteriorDesign.Core/Constants/RoleConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace InteriorDesign.Core.Constants
+{
+    public static class RoleConstants
+    {
+        public const string Employee = "Employee";
+        public const string Administrator = "Administrator";
+    }
+}
