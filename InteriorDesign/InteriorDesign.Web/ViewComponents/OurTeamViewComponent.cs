173dd96b9723e6ca9a1f3e3f3e21c346088ed9fc
mikegsCoder <mikegsid@gmail.com>
2022-11-26 13:19:43 +0200

    Add view component and views for OurTeam

---
+++ InteriorDesign/InteriorDesign.Web/ViewComponents/OurTeamViewComponent.cs
@@ -0,0 +1,40 @@
+﻿using InteriorDesign.Core.Services.Application.OurTeamService;
+using InteriorDesign.Core.ViewModels.OurTeamViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InteriorDesign.Web.ViewComponents
+{
+    [ViewComponent(Name = "OurTeam")]
+    public class OurTeamViewComponent : ViewComponent
+    {
+        private readonly IOurTeamService _ourTeamService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public OurTeamViewComponent(
+            IOurTeamService ourTeamService,
+            IHttpContextAccessor httpContextAccessor)
+        {
+            _ourTeamService = ourTeamService;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            try
+            {
+                // Use this exception to test error handling:
+                //throw new Exception();
+
+                var model = _ourTeamService.GetTeamAsync().GetAwaiter().GetResult();
+
+                return View(model);
+            }
+            catch (Exception)
+            {
+                _httpContextAccessor.HttpContext.Response.Redirect("/Home/ApplicationError");
+
+                return View(new List<OurTeamViewModel>());
+            }
+        }
+    }
+}
