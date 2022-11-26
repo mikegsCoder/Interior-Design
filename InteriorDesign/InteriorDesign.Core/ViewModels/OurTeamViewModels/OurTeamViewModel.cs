c41338841aad9f53bf6c9c65125187b098e03847
mikegsCoder <mikegsid@gmail.com>
2022-11-26 13:15:46 +0200

    Add OurTeamViewModel

---
+++ InteriorDesign/InteriorDesign.Core/ViewModels/OurTeamViewModels/OurTeamViewModel.cs
@@ -0,0 +1,54 @@
+﻿using InteriorDesign.Core.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace InteriorDesign.Core.ViewModels.OurTeamViewModels
+{
+    public class OurTeamViewModel
+    {
+        public string MemberId { get; set; } = null!;
+
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = ViewModelsConstants.RequiredErrorMessage)]
+        [StringLength(
+            ViewModelsConstants.TeamMemberPositionMaxLength,
+            MinimumLength = ViewModelsConstants.TeamMemberPositionMinLength,
+            ErrorMessage = ViewModelsConstants.StringLenghtErrorMessage)]
+        public string Position { get; set; } = null!;
+
+        [Required(ErrorMessage = ViewModelsConstants.RequiredErrorMessage)]
+        [StringLength(
+            ViewModelsConstants.TeamMemberImageUrlMaxLength,
+            MinimumLength = ViewModelsConstants.TeamMemberImageUrlMinLength,
+            ErrorMessage = ViewModelsConstants.StringLenghtErrorMessage)]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required(ErrorMessage = ViewModelsConstants.RequiredErrorMessage)]
+        [StringLength(
+            ViewModelsConstants.TeamMemberSocialUrlMaxLength,
+            MinimumLength = ViewModelsConstants.TeamMemberSocialUrlMinLength,
+            ErrorMessage = ViewModelsConstants.StringLenghtErrorMessage)]
+        public string TwitterUrl { get; set; }
+
+        [Required(ErrorMessage = ViewModelsConstants.RequiredErrorMessage)]
+        [StringLength(
+            ViewModelsConstants.TeamMemberSocialUrlMaxLength,
+            MinimumLength = ViewModelsConstants.TeamMemberSocialUrlMinLength,
+            ErrorMessage = ViewModelsConstants.StringLenghtErrorMessage)]
+        public string FacebookUrl { get; set; }
+
+        [Required(ErrorMessage = ViewModelsConstants.RequiredErrorMessage)]
+        [StringLength(
+            ViewModelsConstants.TeamMemberSocialUrlMaxLength,
+            MinimumLength = ViewModelsConstants.TeamMemberSocialUrlMinLength,
+            ErrorMessage = ViewModelsConstants.StringLenghtErrorMessage)]
+        public string GooglePlusUrl { get; set; }
+
+        [Required(ErrorMessage = ViewModelsConstants.RequiredErrorMessage)]
+        [StringLength(
+            ViewModelsConstants.TeamMemberSocialUrlMaxLength,
+            MinimumLength = ViewModelsConstants.TeamMemberSocialUrlMinLength,
+            ErrorMessage = ViewModelsConstants.StringLenghtErrorMessage)]
+        public string LinkedInUrl { get; set; }
+    }
+}
