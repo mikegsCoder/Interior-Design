11a2d2cfdc4a6edc6095ebbab1278cdd232fa981
mikegsCoder <mikegsid@gmail.com>
2022-11-26 13:06:31 +0200

    Add OurTeamController

---
+++ InteriorDesign/InteriorDesign.Web/Controllers/OurTeamController.cs
@@ -0,0 +1,14 @@
+﻿using InteriorDesign.Core.Services.Application.OurTeamService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InteriorDesign.Web.Controllers
+{
+    public class OurTeamController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
