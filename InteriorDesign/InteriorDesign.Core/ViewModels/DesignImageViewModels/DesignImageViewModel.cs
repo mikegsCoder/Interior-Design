6df39c4c14c8d2fcd0509f1beafa86da7c30de3a
mikegsCoder <mikegsid@gmail.com>
2022-11-27 12:35:14 +0200

    Add DesignImageViewModel

---
+++ InteriorDesign/InteriorDesign.Core/ViewModels/DesignImageViewModels/DesignImageViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace InteriorDesign.Core.ViewModels.DesignImageViewModels
+{
+    public class DesignImageViewModel
+    {
+        public string ImageId { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
