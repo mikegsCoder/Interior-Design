e4298dfb69bc474089bfca0284183210cec3cfb3
mikegsCoder <mikegsid@gmail.com>
2022-11-27 12:08:41 +0200

    Add WebConstants

---
+++ InteriorDesign/InteriorDesign.Core/Constants/WebConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace InteriorDesign.Core.Constants
+{
+    public static class WebConstants
+    {
+        public const string UserError = "/Home/ApplicationError";
+        public const string EmployeeError = "/Employee/Dashboard/EmployeeError";
+        public const string AdminError = "/Administration/Dashboard/AdminError";
+    }
+}
