9e417cba7afe73181e41bf4a90fc73cfa11232e7
mikegsCoder <mikegsid@gmail.com>
2022-11-27 12:27:28 +0200

    Add GalleryController

---
+++ InteriorDesign/InteriorDesign.Web/Controllers/GalleryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InteriorDesign.Web.Controllers
+{
+    public class GalleryController : BaseController
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
