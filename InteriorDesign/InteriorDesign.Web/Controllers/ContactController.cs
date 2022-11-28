9c36200047b846e1630e9b22b85e37ee5ce25e5b
mikegsCoder <mikegsid@gmail.com>
2022-11-28 17:06:12 +0200

    Add ContactController

---
+++ InteriorDesign/InteriorDesign.Web/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using InteriorDesign.Core.Services.Application.UserContactService;
+using InteriorDesign.Core.ViewModels.ContactViewModels;
+using InteriorDesign.Core.Constants;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InteriorDesign.Web.Controllers
+{
+    public class ContactController : BaseController
+    {
+        private readonly IUserContactService _contactService;
+        private readonly ILogger _logger;
+
+        public ContactController(
+            IUserContactService contactService, 
+            ILogger<ContactController> logger)
+        {
+            _contactService = contactService;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index(string sent)
+        {
+            ViewBag.Message = sent;
+
+            return View();
+        }
+    }
+}
