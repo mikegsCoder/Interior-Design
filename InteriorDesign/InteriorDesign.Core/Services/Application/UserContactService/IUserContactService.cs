6fcbb72d2645994fb4b8db9eea31d1d393640fc4
mikegsCoder <mikegsid@gmail.com>
2022-11-28 17:16:26 +0200

    Add UserContactService

---
+++ InteriorDesign/InteriorDesign.Core/Services/Application/UserContactService/IUserContactService.cs
@@ -0,0 +1,9 @@
+﻿using InteriorDesign.Core.ViewModels.ContactViewModels;
+
+namespace InteriorDesign.Core.Services.Application.UserContactService
+{
+    public interface IUserContactService
+    {
+        Task SaveMessageAsync(ContactViewModel model);
+    }
+}
---
+++ InteriorDesign/InteriorDesign.Core/Services/Application/UserContactService/UserContactService.cs
@@ -0,0 +1,33 @@
+﻿using InteriorDesign.Core.ViewModels.ContactViewModels;
+using InteriorDesign.Infrastructure.Data.Models.DataBaseModels;
+using InteriorDesign.Infrastructure.Repositories;
+
+namespace InteriorDesign.Core.Services.Application.UserContactService
+{
+    public class UserContactService : IUserContactService
+    {
+        private readonly IDeletableEntityRepository<Contact> _contacts;
+
+        public UserContactService(IDeletableEntityRepository<Contact> contacts)
+        {
+            _contacts = contacts;
+        }
+
+        public async Task SaveMessageAsync(ContactViewModel model)
+        {
+            var contact = new Contact()
+            {
+                From = model.From,
+                Subject = model.Subject,
+                Message = model.Message,
+                CreatedOn = DateTime.UtcNow,
+                IsAnswered = false,
+                IsDeleted = false
+            };
+
+            await _contacts.AddAsync(contact);
+
+            await _contacts.SaveChangesAsync();
+        }
+    }
+}
