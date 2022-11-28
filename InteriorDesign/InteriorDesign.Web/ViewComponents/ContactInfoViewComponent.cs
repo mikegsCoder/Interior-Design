250c0bfe1e671f79809259b0d811211a949c3fb0
mikegsCoder <mikegsid@gmail.com>
2022-11-28 17:09:20 +0200

    Add ContactInfoViewComponent

---
+++ InteriorDesign/InteriorDesign.Web/ViewComponents/ContactInfoViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InteriorDesign.Web.ViewComponents
+{
+    [ViewComponent(Name = "ContactInfo")]
+    public class ContactInfoViewComponent : ViewComponent
+    {
+        private readonly IConfiguration _configuration;
+
+        public ContactInfoViewComponent(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.Address = _configuration["Contacts:Address"];
+            ViewBag.Phone = _configuration["Contacts:Phone"];
+            ViewBag.Email = _configuration["Contacts:Email"];
+
+            return View();
+        }
+    }
+}
