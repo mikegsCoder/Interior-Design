249e0a1bcb8846545dd2be99d7421da22b5bc6da
mikegsCoder <mikegsid@gmail.com>
2022-11-29 16:52:14 +0200

    Add CategoriesController

---
+++ InteriorDesign/InteriorDesign.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,37 @@
+﻿using InteriorDesign.Core.Services.Application.CategoryService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InteriorDesign.Web.Controllers
+{
+    public class CategoriesController : BaseController
+    {
+        private readonly ICategoryService _service;
+        private readonly ILogger _logger;
+
+        public CategoriesController(
+            ICategoryService service, 
+            ILogger<CategoriesController> logger)
+        {
+            _service = service;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                // Use this exception to test error handling:
+                //throw new Exception("Test Exception");
+
+                var model = await _service.GetCategoriesInfoAsync();
+
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToError(ex, _logger, nameof(CategoriesController), nameof(Index));
+            }
+        }
+    }
+}
