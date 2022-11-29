d54a3358d7d9920249d661e3e41d7a6e887daf5b
mikegsCoder <mikegsid@gmail.com>
2022-11-29 17:05:08 +0200

    Add CategoryTypeViewModel

---
+++ InteriorDesign/InteriorDesign.Core/ViewModels/CategoryTypeViewModels/CategoryTypeViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace InteriorDesign.Core.ViewModels.CategoryTypeViewModels
+{
+    public class CategoryTypeViewModel
+    {
+        public string CategoryName { get; set; }
+
+        public string TypeName { get; set; }
+
+        public int ProductModelsCount { get; set; }
+
+        public int ProductsCount { get; set; }
+
+        public string CategoryImageUrl { get; set; }
+
+        public string TypeImageUrl { get; set; }
+    }
+}
