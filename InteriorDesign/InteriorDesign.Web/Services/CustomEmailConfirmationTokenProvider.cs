e35965747923e2a0dbafea42f505ad5735f46582
mikegsCoder <mikegsid@gmail.com>
2022-11-29 16:46:33 +0200

    Add CustomEmailTokenProvider

---
+++ InteriorDesign/InteriorDesign.Web/Services/CustomEmailConfirmationTokenProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+
+namespace InteriorDesign.Web.Services
+{
+    public class CustomEmailConfirmationTokenProvider<TUser>
+        : DataProtectorTokenProvider<TUser> where TUser : class
+    {
+        public CustomEmailConfirmationTokenProvider(
+            IDataProtectionProvider dataProtectionProvider,
+            IOptions<EmailConfirmationTokenProviderOptions> options,
+            ILogger<DataProtectorTokenProvider<TUser>> logger)
+                : base(dataProtectionProvider, options, logger)
+        {
+        }
+    }
+}
