213309e6a468c36370bd0935581276bb5ab21b44
mikegsCoder <mikegsid@gmail.com>
2022-11-29 16:45:23 +0200

    Add EmailConfirmationTokenProvider

---
+++ InteriorDesign/InteriorDesign.Web/Services/EmailConfirmationTokenProviderOptions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace InteriorDesign.Web.Services
+{
+    public class EmailConfirmationTokenProviderOptions : DataProtectionTokenProviderOptions
+    {
+        public EmailConfirmationTokenProviderOptions()
+        {
+            Name = "EmailDataProtectorTokenProvider";
+            TokenLifespan = TimeSpan.FromHours(4);
+        }
+    }
+}
