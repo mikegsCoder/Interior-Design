99dc3b72aa62024ba9caf244bb21511c5a1a8e5c
mikegsCoder <mikegsid@gmail.com>
2022-11-29 17:03:30 +0200

    Add HttpGet actions for each category

--- InteriorDesign/InteriorDesign.Web/Controllers/CategoriesController.cs
+++ InteriorDesign/InteriorDesign.Web/Controllers/CategoriesController.cs
@@ -33,5 +33,113 @@ namespace InteriorDesign.Web.Controllers
                 return RedirectToError(ex, _logger, nameof(CategoriesController), nameof(Index));
             }
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Garden()
+        {
+            try
+            {
+                // Use this exception to test error handling:
+                //throw new Exception("Test Exception");
+
+                var model = await _service.GetTypesByCategoryInfoAsync("Garden");
+
+                return View("CategoryType", model);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToError(ex, _logger, nameof(CategoriesController), nameof(Garden));
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Kitchen()
+        {
+            try
+            {
+                // Use this exception to test error handling:
+                //throw new Exception("Test Exception");
+
+                var model = await _service.GetTypesByCategoryInfoAsync("Kitchen");
+
+                return View("CategoryType", model);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToError(ex, _logger, nameof(CategoriesController), nameof(Kitchen));
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Office()
+        {
+            try
+            {
+                // Use this exception to test error handling:
+                //throw new Exception("Test Exception");
+
+                var model = await _service.GetTypesByCategoryInfoAsync("Office");
+
+                return View("CategoryType", model);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToError(ex, _logger, nameof(CategoriesController), nameof(Office));
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> BedRoom()
+        {
+            try
+            {
+                // Use this exception to test error handling:
+                //throw new Exception("Test Exception");
+
+                var model = await _service.GetTypesByCategoryInfoAsync("Bed Room");
+
+                return View("CategoryType", model);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToError(ex, _logger, nameof(CategoriesController), nameof(BedRoom));
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> LivingRoom()
+        {
+            try
+            {
+                // Use this exception to test error handling:
+                //throw new Exception("Test Exception");
+
+                var model = await _service.GetTypesByCategoryInfoAsync("Living Room");
+
+                return View("CategoryType", model);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToError(ex, _logger, nameof(CategoriesController), nameof(LivingRoom));
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> YoungRoom()
+        {
+            try
+            {
+                // Use this exception to test error handling:
+                //throw new Exception("Test Exception");
+
+                var model = await _service.GetTypesByCategoryInfoAsync("Young Room");
+
+                return View("CategoryType", model);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToError(ex, _logger, nameof(CategoriesController), nameof(YoungRoom));
+            }
+        }
     }
 }
