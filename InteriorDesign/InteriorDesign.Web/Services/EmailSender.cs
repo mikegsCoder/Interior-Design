230ed11995d8e5f3237a441cb3f4feed3a1e67e5
mikegsCoder <mikegsid@gmail.com>
2022-11-29 16:48:04 +0200

    Add EmailSender

---
+++ InteriorDesign/InteriorDesign.Web/Services/EmailSender.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace InteriorDesign.Web.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly ILogger _logger;
+        private readonly IConfiguration _configuration;
+
+        public EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor,
+                           ILogger<EmailSender> logger,
+                           IConfiguration configuration)
+        {
+            Options = optionsAccessor.Value;
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public AuthMessageSenderOptions Options { get; }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string message)
+        {
+            if (string.IsNullOrEmpty(Options.SendGridKey))
+            {
+                throw new Exception("Null SendGridKey");
+            }
+
+            await Execute(Options.SendGridKey, subject, message, toEmail);
+        }
+
+        public async Task Execute(string apiKey, string subject, string message, string toEmail)
+        {
+            var client = new SendGridClient(apiKey);
+            var email = _configuration["Smtp:Email"];
+
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress(email, "Confirmation email"),
+                Subject = subject,
+                PlainTextContent = message,
+                HtmlContent = message
+            };
+
+            msg.AddTo(new EmailAddress(toEmail));
+
+            // Disable click tracking.
+            // See https://sendgrid.com/docs/User_Guide/Settings/tracking.html
+            msg.SetClickTracking(false, false);
+            var response = await client.SendEmailAsync(msg);
+            _logger.LogInformation(response.IsSuccessStatusCode
+                                   ? $"Email to {toEmail} queued successfully!"
+                                   : $"Failure Email to {toEmail}");
+        }
+    }
+}
