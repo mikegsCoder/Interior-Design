230d97e22661fb6bb52e38714eb490a6ca0a985e
mikegsCoder <mikegsid@gmail.com>
2022-11-30 18:18:46 +0200

    Add MemoryCache

--- InteriorDesign/InteriorDesign.Web/ViewComponents/OurTeamViewComponent.cs
+++ InteriorDesign/InteriorDesign.Web/ViewComponents/OurTeamViewComponent.cs
@@ -1,5 +1,6 @@
 ﻿using InteriorDesign.Core.Services.Application.OurTeamService;
 using InteriorDesign.Core.ViewModels.OurTeamViewModels;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InteriorDesign.Web.ViewComponents
@@ -10,36 +11,62 @@ namespace InteriorDesign.Web.ViewComponents
         private readonly IOurTeamService _ourTeamService;
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly ILogger _logger;
+        private readonly IMemoryCache _cache;
 
         public OurTeamViewComponent(
             IOurTeamService ourTeamService,
             IHttpContextAccessor httpContextAccessor,
-            ILogger<OurTeamViewComponent> logger)
+            ILogger<OurTeamViewComponent> logger,
+            IMemoryCache cache)
         {
             _ourTeamService = ourTeamService;
             _httpContextAccessor = httpContextAccessor;
             _logger = logger;
+            _cache= cache;
         }
 
         public IViewComponentResult Invoke()
         {
-            try
+            if (!_cache.TryGetValue<IEnumerable<OurTeamViewModel>>("OurTeam", out var model))
             {
-                // Use this exception to test error handling:
-                //throw new Exception("Test Exception");
+                try
+                {
+                    // Use this exception to test error handling:
+                    //throw new Exception("Test Exception");
 
-                var model = _ourTeamService.GetTeamAsync().GetAwaiter().GetResult();
+                    model = _ourTeamService.GetTeamAsync().GetAwaiter().GetResult();
 
-                return View(model);
+                    _cache.Set("OurTeam", model, TimeSpan.FromMinutes(5));
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(string.Concat(nameof(OurTeamViewComponent), ": ", ex.Message), ex);
+
+                    _httpContextAccessor?.HttpContext?.Response.Redirect("/Home/ApplicationError");
+
+                    return View(new List<OurTeamViewModel>());
+                }
             }
-            catch (Exception ex)
-            {
-                _logger.LogError(string.Concat(nameof(OurTeamViewComponent), ": ", ex.Message), ex);
+
+            return View(model);
+
+            //try
+            //{
+            //    // Use this exception to test error handling:
+            //    //throw new Exception("Test Exception");
+
+            //    var model = _ourTeamService.GetTeamAsync().GetAwaiter().GetResult();
+
+            //    return View(model);
+            //}
+            //catch (Exception ex)
+            //{
+            //    _logger.LogError(string.Concat(nameof(OurTeamViewComponent), ": ", ex.Message), ex);
                 
-                _httpContextAccessor?.HttpContext?.Response.Redirect("/Home/ApplicationError");
+            //    _httpContextAccessor?.HttpContext?.Response.Redirect("/Home/ApplicationError");
 
-                return View(new List<OurTeamViewModel>());
-            }
+            //    return View(new List<OurTeamViewModel>());
+            //}
         }
     }
 }
