3af5d23ed8dd6e5e254927608ca325732d1a72a6
mikegsCoder <mikegsid@gmail.com>
2022-11-30 18:28:14 +0200

    Add TypeService

---
+++ InteriorDesign/InteriorDesign.Core/Services/Application/TypeService/ITypeService.cs
@@ -0,0 +1,10 @@
+﻿using InteriorDesign.Core.ViewModels.CategoryTypeViewModels;
+using InteriorDesign.Core.ViewModels.TypeViewModels;
+
+namespace InteriorDesign.Core.Services.Application.TypeService
+{
+    public interface ITypeService
+    {
+        Task<IEnumerable<TypeViewModel>> GetTypesInfoAsync();
+    }
+}
---
+++ InteriorDesign/InteriorDesign.Core/Services/Application/TypeService/TypeService.cs
@@ -0,0 +1,65 @@
+﻿using InteriorDesign.Core.ViewModels.CategoryTypeViewModels;
+using InteriorDesign.Core.ViewModels.TypeViewModels;
+using InteriorDesign.Infrastructure.Data.Models.DataBaseModels;
+using InteriorDesign.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace InteriorDesign.Core.Services.Application.TypeService
+{
+    public class TypeService : ITypeService
+    {
+        private readonly IDeletableEntityRepository<ProductCategory> _categories;
+        private readonly IDeletableEntityRepository<ProductType> _types;
+        private readonly IDeletableEntityRepository<ProductModel> _models;
+        private readonly IDeletableEntityRepository<Product> _products;
+
+        public TypeService(IDeletableEntityRepository<ProductCategory> categories,
+            IDeletableEntityRepository<ProductType> types,
+            IDeletableEntityRepository<ProductModel> models,
+            IDeletableEntityRepository<Product> products)
+        {
+            _categories = categories;
+            _types = types;
+            _models = models;
+            _products = products;
+        }
+
+        public async Task<IEnumerable<TypeViewModel>> GetTypesInfoAsync()
+        {
+            var typesInfo = new List<TypeViewModel>();
+
+            var types = await _types.AllAsNoTracking()
+                .Select(t => t.Name)
+                .ToListAsync();
+
+            foreach (var type in types)
+            {
+                var categories = await _categories.AllAsNoTracking()
+                    .Where(c => c.ProductTypes.Any(x => x.Name == type) && !c.IsDeleted)
+                    .CountAsync();
+
+                var models = await _models.AllAsNoTracking()
+                    .Where(m => m.Type.Name == type)
+                    .ToListAsync();
+
+                var products = await _products.AllAsNoTracking()
+                    .Where(p => models.Contains(p.Model))
+                    .CountAsync();
+
+                var firstProduct = await _products.AllAsNoTracking()
+                    .FirstOrDefaultAsync(p => models.Contains(p.Model));
+
+                typesInfo.Add(new TypeViewModel
+                {
+                    Name = type,
+                    ProductCategoriesCount = categories,
+                    ProductModelsCount = models.Count(),
+                    ProductsCount = products,
+                    ImageUrl = firstProduct.ImageUrl
+                });
+            }
+
+            return typesInfo;
+        }
+    }
+}
