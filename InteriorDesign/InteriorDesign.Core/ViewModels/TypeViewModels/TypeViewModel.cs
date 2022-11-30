84b506432d0cfd3cb1ceb9bc1f81a5a8bbd30e78
mikegsCoder <mikegsid@gmail.com>
2022-11-30 18:25:59 +0200

    Add TypeViewModel

---
+++ InteriorDesign/InteriorDesign.Core/ViewModels/TypeViewModels/TypeViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace InteriorDesign.Core.ViewModels.TypeViewModels
+{
+    public class TypeViewModel
+    {
+        public string Name { get; set; } = null!;
+
+        public int ProductCategoriesCount { get; set; }
+
+        public int ProductModelsCount { get; set; }
+
+        public int ProductsCount { get; set; }
+
+        public string ImageUrl { get; set; } = null!;
+    }
+}
