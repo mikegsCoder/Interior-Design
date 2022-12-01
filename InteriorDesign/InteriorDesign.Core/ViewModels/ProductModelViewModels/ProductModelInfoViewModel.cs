31085fa06acd6fd0432bafe12625d91ad19208e5
mikegsCoder <mikegsid@gmail.com>
2022-12-01 19:46:31 +0200

    Add ProductModelInfoViewModel

---
+++ InteriorDesign/InteriorDesign.Core/ViewModels/ProductModelViewModels/ProductModelInfoViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InteriorDesign.Core.ViewModels.ProductModelViewModels
+{
+    public class ProductModelInfoViewModel
+    {
+        public string Id { get; set; }
+
+        public string ModelName { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string TypeName { get; set; }
+
+        public int ProductsCount { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
