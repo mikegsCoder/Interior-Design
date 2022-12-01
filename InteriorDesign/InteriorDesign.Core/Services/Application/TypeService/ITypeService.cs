97f64dbf8b970f18094b6dc26d0dfee339d342f8
mikegsCoder <mikegsid@gmail.com>
2022-12-01 19:37:00 +0200

    Add GetCategoriesByTypeInfoAsync

--- InteriorDesign/InteriorDesign.Core/Services/Application/TypeService/ITypeService.cs
+++ InteriorDesign/InteriorDesign.Core/Services/Application/TypeService/ITypeService.cs
@@ -6,5 +6,7 @@ namespace InteriorDesign.Core.Services.Application.TypeService
     public interface ITypeService
     {
         Task<IEnumerable<TypeViewModel>> GetTypesInfoAsync();
+
+        Task<IEnumerable<CategoryTypeViewModel>> GetCategoriesByTypeInfoAsync(string typeName);
     }
 }
--- InteriorDesign/InteriorDesign.Core/Services/Application/TypeService/TypeService.cs
+++ InteriorDesign/InteriorDesign.Core/Services/Application/TypeService/TypeService.cs
@@ -61,5 +61,43 @@ namespace InteriorDesign.Core.Services.Application.TypeService
 
             return typesInfo;
         }
+
+        public async Task<IEnumerable<CategoryTypeViewModel>> GetCategoriesByTypeInfoAsync(string typeName)
+        {
+            var categoryTypesInfo = new List<CategoryTypeViewModel>();
+
+            var type = await _types.AllAsNoTracking()
+                .FirstOrDefaultAsync(t => t.Name == typeName);
+
+            var categories = await _categories.AllAsNoTracking()
+                .Where(c => c.ProductTypes.Contains(type))
+                .ToListAsync();
+
+            foreach (var category in categories)
+            {
+                var models = await _models.AllAsNoTracking()
+                   .Where(m => m.Category.Name == category.Name && m.Type == type)
+                   .ToListAsync();
+
+                var products = await _products.AllAsNoTracking()
+                    .Where(p => models.Contains(p.Model))
+                    .CountAsync();
+
+                var firstProduct = await _products.AllAsNoTracking()
+                    .FirstOrDefaultAsync(p => models.Contains(p.Model));
+
+                categoryTypesInfo.Add(new CategoryTypeViewModel
+                {
+                    CategoryName = category.Name,
+                    TypeName = type.Name,
+                    ProductModelsCount = models.Count(),
+                    ProductsCount = products,
+                    CategoryImageUrl = category.ImageUrl,
+                    TypeImageUrl = firstProduct.ImageUrl
+                });
+            }
+
+            return categoryTypesInfo;
+        }
     }
 }
