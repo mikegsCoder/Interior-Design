e8ec30ba9f96dc44be003c09b5d8aa934018a767
mikegsCoder <mikegsid@gmail.com>
2022-12-02 16:14:52 +0200

    Add Employee area

---
+++ InteriorDesign/InteriorDesign.Web/Areas/Employee/Controllers/BaseEmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using InteriorDesign.Core.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InteriorDesign.Web.Areas.Employee.Controllers
+{
+    [Area("Employee")]
+    [Authorize(Policy = "RequireEmployeeOnly")]
+    public class BaseEmployeeController : Controller
+    {
+        internal RedirectResult RedirectToError(Exception ex, ILogger logger, string controllerName, string actionName)
+        {
+            logger.LogError(string.Concat(controllerName, " - ", actionName, ": ", ex.Message), ex);
+
+            TempData["Error"] = ex.Message;
+
+            return Redirect(WebConstants.EmployeeError);
+        }
+    }
+}
---
+++ InteriorDesign/InteriorDesign.Web/Areas/Employee/Controllers/DashboardController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InteriorDesign.Web.Areas.Employee.Controllers
+{
+    public class DashboardController : BaseEmployeeController
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult EmployeeError()
+        {
+            return View();
+        }
+    }
+}
