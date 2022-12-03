fd17b0ccaa4d5e6db71ecc009c0c09763d84c74f
mikegsCoder <mikegsid@gmail.com>
2022-12-03 15:12:42 +0200

    Add Dashboard

---
+++ InteriorDesign/InteriorDesign.Web/Areas/Administration/Controllers/DashboardController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InteriorDesign.Web.Areas.Administration.Controllers
+{
+    public class DashboardController : BaseAdminController
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AdminError()
+        {
+            return View();
+        }
+    }
+}
