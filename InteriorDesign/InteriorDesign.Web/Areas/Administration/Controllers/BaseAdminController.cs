74172ef67df3d6cea6e0ca8875affa6beb708ff3
mikegsCoder <mikegsid@gmail.com>
2022-12-03 15:10:30 +0200

    Add Administration area

---
+++ InteriorDesign/InteriorDesign.Web/Areas/Administration/Controllers/BaseAdminController.cs
@@ -0,0 +1,20 @@
+﻿using InteriorDesign.Core.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InteriorDesign.Web.Areas.Administration.Controllers
+{
+    [Area("Administration")]
+    [Authorize(Roles = "Administrator")]
+    public class BaseAdminController : Controller
+    {
+        internal RedirectResult RedirectToError(Exception ex, ILogger logger, string controllerName, string actionName)
+        {
+            logger.LogError(string.Concat(controllerName, " - ", actionName, ": ", ex.Message), ex);
+
+            TempData["Error"] = ex.Message;
+
+            return Redirect(WebConstants.AdminError);
+        }
+    }
+}
