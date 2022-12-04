92f0f5a6614c678e7d5e13c7a48791ffdd321c6b
mikegsCoder <mikegsid@gmail.com>
2022-12-04 12:03:24 +0200

    Add Chat

---
+++ InteriorDesign/InteriorDesign.Web/Areas/Administration/Controllers/ChatController.cs
@@ -0,0 +1,64 @@
+﻿using InteriorDesign.Core.Services.Common.ChatService;
+using InteriorDesign.Core.ViewModels.ChatViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InteriorDesign.Web.Areas.Administration.Controllers
+{
+    public class ChatController : BaseAdminController
+    {
+        private bool isHistoryLoaded = false;
+        private readonly IChatService _chatService;
+        private readonly ILogger _logger;
+
+        public ChatController(
+            IChatService chatService, 
+            ILogger<ChatController> logger)
+        {
+            _chatService = chatService;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            if (isHistoryLoaded)
+            {
+                return View();
+            }
+
+            try
+            {
+                // Use this exception to test error handling:
+                //throw new Exception("Test exception");
+
+                var model = await _chatService.GetAllMessagesAsync();
+
+                isHistoryLoaded = true;
+
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToError(ex, _logger, nameof(ChatController), nameof(Index));
+            }
+        }
+
+        [HttpPost]
+        public async Task<string> SaveMessage(ChatViewModel model)
+        {
+            try
+            {
+                // Use this exception to test error handling:
+                //throw new Exception("Test exception!");
+
+                await _chatService.SaveMessageAsync(model);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(string.Concat(nameof(ChatController), " - ", nameof(SaveMessage), ": ", ex.Message), ex);
+            }
+
+            return null;
+        }
+    }
+}
