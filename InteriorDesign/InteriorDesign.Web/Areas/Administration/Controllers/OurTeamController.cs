27f92e0aad384624e9952c9dfcb030c81897b56c
mikegsCoder <mikegsid@gmail.com>
2022-12-05 18:18:23 +0200

    Add OurTeamController

---
+++ InteriorDesign/InteriorDesign.Web/Areas/Administration/Controllers/OurTeamController.cs
@@ -0,0 +1,26 @@
+﻿using InteriorDesign.Core.Services.Application.OurTeamService;
+using InteriorDesign.Core.ViewModels.OurTeamViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InteriorDesign.Web.Areas.Administration.Controllers
+{
+    public class OurTeamController : BaseAdminController
+    {
+        private readonly IOurTeamService _ourTeamService;
+        private readonly ILogger _logger;
+
+        public OurTeamController(
+            IOurTeamService ourTeamService, 
+            ILogger<OurTeamController> logger)
+        {
+            _ourTeamService = ourTeamService;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
