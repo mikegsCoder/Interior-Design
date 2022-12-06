bb1980fdafe5d3d9d3b7688a7355d3bde262fbc5
mikegsCoder <mikegsid@gmail.com>
2022-12-06 15:59:59 +0200

    Add ProductsController

---
+++ InteriorDesign/InteriorDesign.Web/Areas/Administration/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using InteriorDesign.Core.Services.Application.CartService;
+using InteriorDesign.Core.Services.Application.ProductService;
+using InteriorDesign.Core.ViewModels.ProductViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InteriorDesign.Web.Areas.Administration.Controllers
+{
+    public class ProductsController : BaseAdminController
+    {
+        private readonly IProductService _productService;
+        private readonly ICartService _cartService;
+        private readonly ILogger _logger;
+
+        public ProductsController(
+            IProductService productService,
+            ICartService cartService,
+            ILogger<ProductsController> logger)
+        {
+            _productService = productService;
+            _cartService = cartService;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                // Use this exception to test error handling:
+                //throw new Exception("Test exception");
+
+                var products = await _productService.GetAllProductsAsync();
+
+                return View(products);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToError(ex, _logger, nameof(ProductsController), nameof(Index));
+            }
+        }
+    }
+}
