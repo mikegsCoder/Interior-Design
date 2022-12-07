563ef3ad94e834c393107ac256be2f345cb2ae90
mikegsCoder <mikegsid@gmail.com>
2022-12-07 16:14:32 +0200

    Add TestimonialsController

---
+++ InteriorDesign/InteriorDesign.Web/Areas/Administration/Controllers/TestimonialsController.cs
@@ -0,0 +1,26 @@
+﻿using InteriorDesign.Core.Services.Application.AboutUsService;
+using InteriorDesign.Web.Areas.Administration.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InteriorDesign.Web.Administration.Controllers
+{
+    public class TestimonialsController : BaseAdminController
+    {
+        private readonly IAboutUsService _aboutUsService;
+        private readonly ILogger _logger;
+
+        public TestimonialsController(
+            IAboutUsService aboutUsService, 
+            ILogger<TestimonialsController> logger)
+        {
+            _aboutUsService = aboutUsService;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
