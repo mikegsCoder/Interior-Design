ab0a10f9bb2abfc04a73643a78d9425b0f667e73
mikegsCoder <mikegsid@gmail.com>
2022-12-10 11:30:49 +0200

    Add Types view tests

---
+++ InteriorDesign/InteriorDesign.Tests/WebTests/Types/TypesControllerViewsTests.cs
@@ -0,0 +1,35 @@
+﻿namespace InteriorDesign.Tests.WebTests.Types;
+
+public class TypesControllerViewsTests : WebTest
+{
+    [Fact]
+    public async Task IndexPageReturnsCorrectResult()
+    {
+        var response = await client.GetAsync("/Types");
+        var html = await response.Content.ReadAsStringAsync();
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.Contains("Product types", html);
+        Assert.Contains("product types count:", html);
+        Assert.Contains("<th>Type</th>", html);
+        Assert.Contains("<th>Product categories</th>", html);
+        Assert.Contains("<th>Product models</th>", html);
+        Assert.Contains("<th>Products</th>", html);
+        Assert.Contains("<th>Open</th>", html);
+    }
+
+    [Fact]
+    public async Task CategoryTypePageReturnsCorrectResult()
+    {
+        var response = await client.GetAsync("/Types/Bed");
+        var html = await response.Content.ReadAsStringAsync();
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.Contains("Categories in product type Bed:", html);
+        Assert.Contains("categories count:", html);
+        Assert.Contains("<th>Category</th>", html);
+        Assert.Contains("<th>Product models</th>", html);
+        Assert.Contains("<th>Products</th>", html);
+        Assert.Contains("<th>Open</th>", html);
+    }
+}
