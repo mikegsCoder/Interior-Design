9d35e36820f2313b8ddd128db991985130187e0d
mikegsCoder <mikegsid@gmail.com>
2022-12-10 11:22:47 +0200

    Add Categories tests

---
+++ InteriorDesign/InteriorDesign.Tests/WebTests/Categories/CategoriesControllerViewsTests.cs
@@ -0,0 +1,33 @@
+﻿namespace InteriorDesign.Tests.WebTests.Categories;
+public class CategoriesControllerViewsTests : WebTest
+{
+    [Fact]
+    public async Task IndexPageReturnsCorrectResult()
+    {
+        var response = await client.GetAsync("/Categories");
+        var html = await response.Content.ReadAsStringAsync();
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.Contains("Categories", html);
+        Assert.Contains("categories count:", html);
+        Assert.Contains("<th>Category</th>", html);
+        Assert.Contains("<th>Product types</th>", html);
+        Assert.Contains("<th>Product models</th>", html);
+        Assert.Contains("<th>Products</th>", html);
+        Assert.Contains("<th>Open</th>", html);
+    }
+
+    [Fact]
+    public async Task CategoryTypePageContainsCorrectText()
+    {
+        var response = await client.GetAsync("/Categories/Garden");
+        var html = await response.Content.ReadAsStringAsync();
+
+        Assert.Contains("Product types in category Garden", html);
+        Assert.Contains("product types count:", html);
+        Assert.Contains("<th>Type</th>", html);
+        Assert.Contains("<th>Product models</th>", html);
+        Assert.Contains("<th>Products</th>", html);
+        Assert.Contains("<th>Open</th>", html);
+    }
+}
