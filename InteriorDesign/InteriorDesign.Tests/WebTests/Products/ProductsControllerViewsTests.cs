e029e362840c8059ea31d1be25c3535393a29c01
mikegsCoder <mikegsid@gmail.com>
2022-12-10 11:30:00 +0200

    Add Products view test

---
+++ InteriorDesign/InteriorDesign.Tests/WebTests/Products/ProductsControllerViewsTests.cs
@@ -0,0 +1,22 @@
+﻿namespace InteriorDesign.Tests.WebTests.Products;
+
+public class ProductsControllerViewsTests : WebTest
+{
+    [Fact]
+    public async Task IndexPageReturnsCorrectResult()
+    {
+        var response = await client.GetAsync("/Products");
+        var html = await response.Content.ReadAsStringAsync();
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.Contains("All Products", html);
+        Assert.Contains("products count:", html);
+        Assert.Contains("<th>Product</th>", html);
+        Assert.Contains("<th>Category</th>", html);
+        Assert.Contains("<th>Type</th>", html);
+        Assert.Contains("<th>Model</th>", html);
+        Assert.Contains("<th>Color</th>", html);
+        Assert.Contains("<th>Price</th>", html);
+        Assert.Contains("<th>Details</th>", html);
+    }
+}
