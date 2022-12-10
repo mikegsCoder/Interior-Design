9b9bcc7cf4ee0f42c864ba0e486583918100974a
mikegsCoder <mikegsid@gmail.com>
2022-12-10 11:24:39 +0200

    Add Gallery test

---
+++ InteriorDesign/InteriorDesign.Tests/WebTests/Gallery/GalleryControllerViewsTests.cs
@@ -0,0 +1,15 @@
+﻿namespace InteriorDesign.Tests.WebTests.Gallery;
+
+public class GalleryControllerViewsTests : WebTest
+{
+    [Fact]
+    public async Task IndexPageReturnsCorrectResult()
+    {
+        var response = await client.GetAsync("/Gallery");
+        var html = await response.Content.ReadAsStringAsync();
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.Contains("Here you can see some examples of rooms furnished with our products.", html);
+        Assert.Contains("Enjoy the art of Interior Design.", html);
+    }
+}
