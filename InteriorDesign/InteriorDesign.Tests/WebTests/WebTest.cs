7a9a315bb53a6fb7ecb5a922273f9615aae3dd21
mikegsCoder <mikegsid@gmail.com>
2022-12-10 11:20:03 +0200

    Add WebTest

---
+++ InteriorDesign/InteriorDesign.Tests/WebTests/WebTest.cs
@@ -0,0 +1,12 @@
+﻿namespace InteriorDesign.Tests.WebTests;
+public class WebTest : WebApplicationFactory<Program>
+{
+    private readonly WebApplicationFactory<Program> application;
+    internal readonly HttpClient client;
+
+    public WebTest()
+    {
+        application = new WebApplicationFactory<Program>();
+        client = application.CreateClient();
+    }
+}
