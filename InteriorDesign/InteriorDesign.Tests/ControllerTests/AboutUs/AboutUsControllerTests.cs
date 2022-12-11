85f8ea6c6c344ad150f3e8d6dda55d739cf7a34a
mikegsCoder <mikegsid@gmail.com>
2022-12-11 11:45:13 +0200

    Add AboutUsControllerTests

---
+++ InteriorDesign/InteriorDesign.Tests/ControllerTests/AboutUs/AboutUsControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using InteriorDesign.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InteriorDesign.Tests.ControllerTests.AboutUs
+{
+    public class AboutUsControllerTests
+    {
+        [Fact]
+        public void IndexReturnsViewResult()
+        {
+            var controller = new AboutUsController();
+
+            var result = controller.Index();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.NotNull(viewResult);
+            Assert.Null(viewResult.ViewData.Model);
+            Assert.Null(viewResult.ViewName);
+            Assert.Null(viewResult.StatusCode);
+        }
+    }
+}
