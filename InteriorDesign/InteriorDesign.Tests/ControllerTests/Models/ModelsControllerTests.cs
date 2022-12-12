b1a62fc24eba48a65a5a42000acb9cf4cf6806a7
mikegsCoder <mikegsid@gmail.com>
2022-12-12 12:52:19 +0200

    Add ModelsControllerTests

---
+++ InteriorDesign/InteriorDesign.Tests/ControllerTests/Models/ModelsControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using InteriorDesign.Core.Services.Application.ModelService;
+using InteriorDesign.Core.ViewModels.ProductModelViewModels;
+using InteriorDesign.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+
+namespace InteriorDesign.Tests.ControllerTests.ModelsControllerTests
+{
+    public class ModelsControllerTests
+    {
+        private readonly List<ProductModelInfoViewModel> model;
+
+        private readonly Mock<ILogger<ModelsController>> logger;
+
+        private readonly Mock<IMemoryCache> cache;
+
+        private readonly Mock<IModelService> service;
+
+        private object testModel;
+
+        public ModelsControllerTests()
+        {
+            logger = new Mock<ILogger<ModelsController>>();
+
+            cache = new Mock<IMemoryCache>();
+
+            service = new Mock<IModelService>();
+        }
+
+        [Fact]
+        public async Task BedRoom_Bed_ReturnsRedirectResultToApplicationError()
+        {
+            var controller = new ModelsController(
+                service.Object,
+                logger.Object,
+                cache.Object);
+
+            var result = await controller.BedRoom_Bed();
+
+            var redirectResult = Assert.IsType<RedirectResult>(result);
+            Assert.Equal("/Home/ApplicationError", redirectResult.Url);
+        }
+    }
+}
