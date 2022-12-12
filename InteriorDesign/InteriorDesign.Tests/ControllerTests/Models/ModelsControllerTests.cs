8434740dafa0ac2049bf31a161783878b4e96cfb
mikegsCoder <mikegsid@gmail.com>
2022-12-12 12:55:57 +0200

    Add new test

--- InteriorDesign/InteriorDesign.Tests/ControllerTests/Models/ModelsControllerTests.cs
+++ InteriorDesign/InteriorDesign.Tests/ControllerTests/Models/ModelsControllerTests.cs
@@ -91,5 +91,19 @@ namespace InteriorDesign.Tests.ControllerTests.ModelsControllerTests
             Assert.Equal("CategoryTypeModels", viewResult.ViewName);
             Assert.Null(viewResult.Model);
         }
+
+        [Fact]
+        public async Task BedRoom_Wardrobe_ReturnsRedirectResultToApplicationError()
+        {
+            var controller = new ModelsController(
+                service.Object,
+                logger.Object,
+                cache.Object);
+
+            var result = await controller.BedRoom_Wardrobe();
+
+            var redirectResult = Assert.IsType<RedirectResult>(result);
+            Assert.Equal("/Home/ApplicationError", redirectResult.Url);
+        }
     }
 }
