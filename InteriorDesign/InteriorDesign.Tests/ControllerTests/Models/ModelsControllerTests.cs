cd454778e46a017b05c27d12d0299c16662499ce
mikegsCoder <mikegsid@gmail.com>
2022-12-13 13:52:57 +0200

    Add new test

--- InteriorDesign/InteriorDesign.Tests/ControllerTests/Models/ModelsControllerTests.cs
+++ InteriorDesign/InteriorDesign.Tests/ControllerTests/Models/ModelsControllerTests.cs
@@ -315,5 +315,19 @@ namespace InteriorDesign.Tests.ControllerTests.ModelsControllerTests
             Assert.Equal("CategoryTypeModels", viewResult.ViewName);
             Assert.Null(viewResult.Model);
         }
+
+        [Fact]
+        public async Task LivingRoom_Chair_ReturnsRedirectResultToApplicationError()
+        {
+            var controller = new ModelsController(
+                service.Object,
+                logger.Object,
+                cache.Object);
+
+            var result = await controller.LivingRoom_Chair();
+
+            var redirectResult = Assert.IsType<RedirectResult>(result);
+            Assert.Equal("/Home/ApplicationError", redirectResult.Url);
+        }
     }
 }
