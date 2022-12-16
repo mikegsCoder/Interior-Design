48eac1845a4685a15b5c59f96193981ce60c09c6
mikegsCoder <mikegsid@gmail.com>
2022-12-16 12:25:12 +0200

    Add new test

--- InteriorDesign/InteriorDesign.Tests/ControllerTests/Types/TypesControllerTests.cs
+++ InteriorDesign/InteriorDesign.Tests/ControllerTests/Types/TypesControllerTests.cs
@@ -102,5 +102,23 @@ namespace InteriorDesign.Tests.ControllerTests.TypesControllerTests
             var redirectResult = Assert.IsType<RedirectResult>(result);
             Assert.Equal("/Home/ApplicationError", redirectResult.Url);
         }
+
+        [Fact]
+        public async Task DeskReturnsViewResultWhenGetModelFromCache()
+        {
+            cache.Setup(x => x.TryGetValue(It.IsAny<object>(), out testModel))
+                .Returns(true);
+
+            var controller = new TypesController(
+                service.Object,
+                logger.Object,
+                cache.Object);
+
+            var result = await controller.Desk();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Equal("TypeCategory", viewResult.ViewName);
+            Assert.Null(viewResult.Model);
+        }
     }
 }
