4c8c342a832a7b74ad44d40004f2a59f4387df46
mikegsCoder <mikegsid@gmail.com>
2022-12-16 12:15:02 +0200

    Add mew test

--- InteriorDesign/InteriorDesign.Tests/ControllerTests/Models/ModelsControllerTests.cs
+++ InteriorDesign/InteriorDesign.Tests/ControllerTests/Models/ModelsControllerTests.cs
@@ -699,5 +699,19 @@ namespace InteriorDesign.Tests.ControllerTests.ModelsControllerTests
             Assert.Equal("CategoryTypeModels", viewResult.ViewName);
             Assert.Null(viewResult.Model);
         }
+
+        [Fact]
+        public async Task YoungRoom_Wardrobe_ReturnsRedirectResultToApplicationError()
+        {
+            var controller = new ModelsController(
+                service.Object,
+                logger.Object,
+                cache.Object);
+
+            var result = await controller.YoungRoom_Wardrobe();
+
+            var redirectResult = Assert.IsType<RedirectResult>(result);
+            Assert.Equal("/Home/ApplicationError", redirectResult.Url);
+        }
     }
 }
